dd196a6601a45ee9695a327d6d7c126656bc606f
Abhishek Gupta <abhishekmodi2011@gmail.com>
2017-06-01 04:18:03 +0530

    Fix for math_test

--- math_test.py
+++ math_test.py
@@ -4,4 +4,4 @@ from src.math import Math
 class MathTest(unittest.TestCase):
     def test_add_integers(self):
         #To make test fail
-        self.assertEqual(Math.add_integers(2,9), 10)
+        self.assertEqual(Math.add_integers(2,9), 11)
